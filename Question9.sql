4453cd08de5fdb775bacc0438b02607d03eb1b8e
gauravdave083 <gd836398@gmail.com>
2024-11-04 13:47:32 +0530

    Changes

---
+++ Question9.sql
@@ -0,0 +1,17 @@
+/*
+
+Weather Observation Station 4:
+
+Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
+
+The STATION table is described as follows:
+
+ID: Number
+CITY: VARCHAR2(21)
+STATE: VARCHAR2(2)
+LAT_N: Number
+LONG_W: Number
+
+*/
+
+SELECT ((SELECT COUNT(CITY) FROM STATION ) - (SELECT COUNT(DISTINCT CITY) FROM STATION));
