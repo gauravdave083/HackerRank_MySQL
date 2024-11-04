a44fbc4ff2e284879c6841846fd1e9363bcd1006
gauravdave083 <gd836398@gmail.com>
2024-11-04 12:15:01 +0530

    CHANGES

---
+++ Question3.sql
@@ -0,0 +1,17 @@
+/*
+
+SELECT ALL:
+
+Query all columns (attributes) for every row in the CITY table.
+
+Database schema for a table named CITY. It has the following columns:
+
+ID: Number
+NAME: VARCHAR2(17)
+COUNTRYCODE: VARCHAR2(3)
+DISTRICT: VARCHAR2(20)
+POPULATION: Number
+
+*/
+
+SELECT * FROM CITY;
